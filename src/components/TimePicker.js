159f7df4585b3097db55af019f49ea877acbb9c0
Julio Sampaio <juliodssampaio@gmail.com>
2018-02-18 00:48:21 -0300

    Add Default Theme

--- src/components/TimePicker.js
+++ src/components/TimePicker.js
@@ -1,23 +1,34 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 import { media } from '../utils/media'
 
-const TimePicker = () =>
-  <Wrapper>
-  </Wrapper>
+const TimePicker = ({ theme }) =>
+  <ThemeProvider theme={theme || DefaultTheme} >
+    <Wrapper>
+    </Wrapper>
+  </ThemeProvider>
 
 const Wrapper = styled.div`
   position: absolute;
   top: 0; left: 0; bottom: 0; right: 0;
   margin: auto;
-  background: #F8F8F8;
+  background: ${props => props.theme.background};
   border-radius: 3px;
-  box-shadow: 0px 5px 35px #828282;
-  width: 300px;
-  height: 400px;
+  box-shadow: 0px 5px 35px ${props => props.theme.boxShadowColor};
+  width: ${props => props.theme.width}px;
+  height: ${props => props.theme.height}px;
   ${media.landscape`
-    width: 400px;
-    height: 300px;
+    width: ${props => props.theme.landscapeWidth}px;
+    height: ${props => props.theme.landscapeHeight}px;
   `}
 `
+
+const DefaultTheme = {
+  background: '#F8F8F8',
+  boxShadowColor: '#828282',
+  height: 400,
+  landscapeWidth: 400,
+  landscapeHeight: 300,
+  width: 300,
+}
 export default TimePicker
