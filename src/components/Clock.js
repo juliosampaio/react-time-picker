bb0e97401e542fc787c25e468ae79218b82b049f
Julio Sampaio <juliodssampaio@gmail.com>
2018-02-24 18:43:49 -0300

    Add digits across the watch

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,20 +1,33 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export default class Clock extends React.Component {
   state = {
-    hours: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-    minutes: ['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55']
+    hours: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2],
+    minutes: [
+      "15","16","17","18","19","20","21","22","23","24",
+      "25","26","27","28","29","30","31","32","33","34",
+      "35","36","37","38","39","40","41","42","43","44",
+      "45","46","47","48","49","50","51","52","53","54",
+      "55","56","57","58","59","00","01","02","03","04",
+      "05","06","07","08","09","10","11","12","13","14"
+    ]
   }
   render(){
     return (
       <ClockWrapper>
-        <HoursContainer>
-          {this.state.hours.map((i) => <Digit key={i}>{i}</Digit>)}
-          <MinutesContainer>
-            {this.state.minutes.map((i) => <Digit key={i}>{i}</Digit>)}
-          </MinutesContainer>
-        </HoursContainer>
+        <DigitsContainer active>
+          {this.state.hours.map((d, idx) => <Digit deg={idx * 30} key={d} major>{d}</Digit>)}
+        </DigitsContainer>
+        <DigitsContainer>
+          {this.state.minutes.map((d, idx) =>
+            <Digit
+              deg={idx * 6}
+              key={d}
+              major={0+d % 5 === 0 }
+            >{d}</Digit>
+          )}
+        </DigitsContainer>
       </ClockWrapper>
     )
   }
@@ -30,7 +43,8 @@ const ClockWrapper = styled.div`
   justify-content: center;
   align-items: center;
 `
-const HoursContainer = styled.ul`
+const DigitsContainer = styled.ul`
+  display: ${props => props.active ? 'block' : 'none'};
   padding: 0;
   margin: 0;
   list-style: none;
@@ -38,47 +52,49 @@ const HoursContainer = styled.ul`
   height: 100%;
   border-radius: 50%;
   position: relative;
-  li:nth-child(1) { transform: translate(60px, -90px); }
-  li:nth-child(2) { transform: translate(100px,-45px); }
-  li:nth-child(3) { transform: translate(110px, 0); }
-  li:nth-child(4) { transform: translate(100px,45px); }
-  li:nth-child(5) { transform: translate(60px,85px) }
-  li:nth-child(6) { transform: translate(5px,100px); }
-  li:nth-child(7) { transform: translate(-50px,85px); }
-  li:nth-child(8) { transform: translate(-90px,45px); }
-  li:nth-child(9) { transform: translate(-100px, 0); }
-  li:nth-child(10) { transform: translate(-90px,-45px) }
-  li:nth-child(11) { transform: translate(-60px,-90px); }
-  li:nth-child(12) { transform: translate(0px, -100px); }
-`
-
-const MinutesContainer = HoursContainer.extend`
-    width: 40%;
-    height: 40%;
-    position: absolute;
-    border: 1px solid;
-    top: 68px;
-    left: 68px;
-    li:nth-child(1) { transform: translate(60px, -90px); }
-    li:nth-child(2) { transform: translate(40px,-45px) }
-    li:nth-child(3) { transform: translate(110px, 0); }
-    li:nth-child(4) { transform: translate(100px,45px); }
-    li:nth-child(5) { transform: translate(60px,85px) }
-    li:nth-child(6) { transform: translate(5px,100px); }
-    li:nth-child(7) { transform: translate(-50px,85px); }
-    li:nth-child(8) { transform: translate(-90px,45px); }
-    li:nth-child(9) { transform: translate(-100px, 0); }
-    li:nth-child(10) { transform: translate(-90px,-45px) }
-    li:nth-child(11) { transform: translate(-50px,-45px) }
-    li:nth-child(12) { transform: translate(0px, -60px); }
 `
 
 const Digit = styled.li`
   color: #696768;
+  cursor: pointer;
   font-weight: bold;
+  font-size: 0;
   display: inline-block;
   position: absolute;
-  top: 46%;
-  left: 46%;
+  width: 30px;
+  height: 30px;
+  margin: -15px;
+  top: 50%;
+  left: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  transform: rotate(${props => props.deg}deg) translate(100px) rotate(${props => props.deg * -1}deg);
+  ${props => props.major && css`
+    font-size: 16px;
+    &:after {
+      display: none;
+    }
+  `}
+  ${props => props.active && css`
+
+  `}
+  &:hover {
+    background-color: #009A8A;
+    color: #ffffff;
+    opacity: 0.9;
+    &:after {
+      content: '';
+      background: #ffffff;
+      width: 6px;
+      height: 6px;
+      margin: -3px;
+      border-radius: 50%;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+    }
+  }
 `
 
